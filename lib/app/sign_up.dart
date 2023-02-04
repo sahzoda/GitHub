59392f9802b6c71d8a333834ccda2311b74cb0e5
sahzoda <shahzodanigmatjanova524@gmail.com>
2023-02-04 16:58:40 +0600

    sign up done

--- lib/app/sign_up.dart
+++ lib/app/sign_up.dart
@@ -7,7 +7,7 @@ const SignUp({ Key? key }) : super(key: key);
   Widget build(BuildContext context){
     return Scaffold(
       appBar: AppBar(
-        title: Text("Sign up"),
+        title: Text("Sign Up"),
       ),
     );
   }
